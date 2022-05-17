c46c65a0c2cc269c9b22ad7fbb69554918354898
Milton Filho <97262497+josemiltonfilho@users.noreply.github.com>
2022-05-17 20:07:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function rollDice() {
+  
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+  document.querySelector(".img1").setAttribute("src", `images/dice${randomNumber1}.png`);
+  document.querySelector(".img2").setAttribute("src", `images/dice${randomNumber2}.png`);
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🏆Player 1🏆";
+  } else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "🏆Player 2🏆";
+  } else {
+    document.querySelector("h1").innerHTML = "Draw!"
+  }
+}
+
+document.addEventListener("click", rollDice);
